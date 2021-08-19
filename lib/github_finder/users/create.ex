e996742ba57cf9ea1009548e7c3360958d1bafbe
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-08-19 14:03:06 -0300

    Created user module to create user

---
+++ lib/github_finder/users/create.ex
@@ -0,0 +1,16 @@
+defmodule GithubFinder.Users.Create do
+  alias Github.{Error, Repo, User}
+
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
+  end
+end
