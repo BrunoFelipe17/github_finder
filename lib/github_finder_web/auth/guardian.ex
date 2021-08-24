9a69d71564abff36098d1b9da94a7987c678a61c
Bruno Felipe <bruno.felipe7170@gmail.com>
2021-08-24 13:45:59 -0300

    Refreshing token

--- lib/github_finder_web/auth/guardian.ex
+++ lib/github_finder_web/auth/guardian.ex
@@ -14,7 +14,7 @@ defmodule GithubFinderWeb.Auth.Guardian do
   def authenticate(%{"id" => id, "password" => password}) do
     with {:ok, %User{password_hash: hash} = user} <- GetUser.by_id(id),
          true <- Pbkdf2.verify_pass(password, hash),
-         {:ok, token, _claim} <- encode_and_sign(user) do
+         {:ok, token, _claim} <- encode_and_sign(user, token_type: "refresh", ttl: {10, :minutes}) do
       {:ok, token}
     else
       false -> {:error, Error.build(:unauthorized, "Please verify your credentials")}
